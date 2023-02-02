e9fe73648b34361f922228c61b6efe8685abd297
Axel LEBAS <axel.lebas@decathlon.com>
2023-02-02 11:50:56 +0100

    refactor: deal entity

--- src/main/java/fr/ulco/dealhunter/models/entities/DealEntity.java
+++ src/main/java/fr/ulco/dealhunter/models/entities/DealEntity.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
@@ -14,9 +16,25 @@ public class DealEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @ManyToOne
     private UserEntity author;
+    @Column(name="title")
     private String title;
+    @Column(name = "description")
     private String description;
+    @Column(name = "original_price")
     private Double originalPrice;
-    private Double dealPrice;
+    @Column(name = "discounted_price")
+    private Double discountedPrice;
+    @Column(name="url")
+    private String url;
+    @Column(name="image_url")
+    private String imageUrl;
+    @Basic
+    @Column(name = "created_at", updatable = false, columnDefinition = "timestamp default current_timestamp")
+    private LocalDateTime createdAt;
+    @Basic
+    @Column(name = "updated_at", columnDefinition = "timestamp default current_timestamp")
+    private LocalDateTime updatedAt;
 }
+
