d74671da701d8fe0335631aa8ec3af799d1e41d4
Axel LEBAS <axel.lebas@decathlon.com>
2023-02-05 16:21:08 +0100

    chore: reset everything

--- src/main/java/fr/ulco/dealhunter/services/DealService.java
+++
@@ -1,8 +0,0 @@
-package fr.ulco.dealhunter.services;
-
-import fr.ulco.dealhunter.models.dto.DealRequest;
-import fr.ulco.dealhunter.models.entities.DealEntity;
-
-public interface DealService {
-    DealEntity createDeal(DealRequest dealRequest);
-}
