32b12cf2876501ce761c89d92bf743bc85b473af
Axel LEBAS <axel.lebas@decathlon.com>
2023-02-05 16:47:37 +0100

    chore: init project

---
+++ src/main/java/fr/ulco/dealhunter/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package fr.ulco.dealhunter.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/fr/ulco/dealhunter/DealHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ulco.dealhunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealHunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
