3e036efe5115bc61f072b338bc1cfc2497e1eb94
Axel LEBAS <axel.lebas@decathlon.com>
2023-02-16 21:58:13 +0100

    chore: removing useless DealService interface

--- src/main/java/fr/ulco/dealhunter/services/DealService.java
+++ src/main/java/fr/ulco/dealhunter/services/DealService.java
@@ -3,15 +3,51 @@ package fr.ulco.dealhunter.services;
 import fr.ulco.dealhunter.models.dto.CreateDealRequestDto;
 import fr.ulco.dealhunter.models.dto.DealResponseDto;
 import fr.ulco.dealhunter.models.dto.UpdateDealRequestDto;
-import jakarta.persistence.EntityNotFoundException;
+import fr.ulco.dealhunter.models.entities.DealEntity;
+import fr.ulco.dealhunter.models.mappers.DealMapper;
+import fr.ulco.dealhunter.repositories.DealRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface DealService {
-    DealResponseDto create(CreateDealRequestDto deal);
-    DealResponseDto update(UUID uuid, UpdateDealRequestDto deal);
-    DealResponseDto get(UUID id) throws EntityNotFoundException;
-    List<DealResponseDto> getAll();
-    void delete(UUID id);
+@Service @RequiredArgsConstructor
+public class DealService {
+    private final DealRepository dealRepository;
+    private final DealMapper dealMapper;
+
+    public DealResponseDto create(CreateDealRequestDto deal) {
+        DealEntity dealEntity = dealMapper.toEntity(deal);
+        dealRepository.save(dealEntity);
+        return dealMapper.toDto(dealEntity);
+    }
+
+    public DealResponseDto update(UUID uuid, UpdateDealRequestDto deal) throws ResponseStatusException {
+        DealEntity dealEntity = dealRepository.findById(uuid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Deal to update not found"));
+        dealMapper.updateEntity(deal, dealEntity);
+        dealRepository.save(dealEntity);
+        return dealMapper.toDto(dealEntity);
+    }
+
+    public DealResponseDto get(UUID id) throws ResponseStatusException {
+        DealEntity dealEntity = dealRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Deal not found"));
+        return dealMapper.toDto(dealEntity);
+    }
+
+    public List<DealResponseDto> getAll() {
+        List<DealEntity> dealEntityList = dealRepository.findAll();
+        return dealEntityList.stream().map(dealMapper::toDto).toList();
+    }
+
+    public void delete(UUID id) throws ResponseStatusException {
+        try {
+            dealRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Deal to delete not found");
+        }
+    }
 }
