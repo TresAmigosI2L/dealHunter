f0416a7839ff1b582b9d8d876506ba878d785d8b
Vitse Maxime <maxime.vitse@decathlon.com>
2023-03-08 13:48:22 +0100

    SecurityConfig

--- src/main/java/fr/ulco/dealhunter/DealHunterApplication.java
+++ src/main/java/fr/ulco/dealhunter/DealHunterApplication.java
@@ -2,8 +2,9 @@ package fr.ulco.dealhunter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class DealHunterApplication {
 
     public static void main(String[] args) {
