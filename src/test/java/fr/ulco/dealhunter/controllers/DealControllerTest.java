17d3d9632506d794d62cecabab6f7cb74d0cd9d3
Wassim DJAMAA <56625264+WassimDjamaa@users.noreply.github.com>
2023-03-09 11:33:33 +0100

    feat : add test

--- src/test/java/fr/ulco/dealhunter/controllers/DealControllerTest.java
+++ src/test/java/fr/ulco/dealhunter/controllers/DealControllerTest.java
@@ -1,39 +1,42 @@
 package fr.ulco.dealhunter.controllers;
 
 import fr.ulco.dealhunter.models.dto.DealResponseDto;
+import fr.ulco.dealhunter.models.entities.DealEntity;
+import fr.ulco.dealhunter.repositories.DealRepository;
 import fr.ulco.dealhunter.services.DealService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.List;
-import java.util.UUID;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 // FIXME: This test fails when DB is off, it shouldn't
 @SpringBootTest
-@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
 class DealControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Mock
+    private DealRepository dealRepository;
+
+   // @Mock
+    //private DealService dealService;
+
     @InjectMocks
     private DealController dealController;
-    @Mock
-    private DealService dealService;
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders.standaloneSetup(dealController).build();
-    }
 
     @Test
     void getDealsEmpty() throws Exception {
@@ -47,15 +50,32 @@ class DealControllerTest {
     @Test
     void getDeals() throws Exception {
         UUID id = UUID.randomUUID();
-        DealResponseDto deal = new DealResponseDto();
-        deal.setId(id);
 
-        when(dealService.getAll()).thenReturn(List.of(deal));
+        DealEntity dealEntity = new DealEntity();
+        dealEntity.setId(id);
+        dealEntity.setTitle("title");
+        dealEntity.setActive(true);
 
-        mockMvc.perform(get("/api/deals"))
+        DealResponseDto dealResponseDto = new DealResponseDto();
+        dealResponseDto.setId(id);
+        dealResponseDto.setTitle("title");
+        dealResponseDto.setActive(true);
+
+        when(dealRepository.findAll()).thenReturn(Arrays.asList(dealEntity));
+        //when(dealService.getAll()).thenReturn(Arrays.asList(dealResponseDto));
+
+
+        final var basicPayload = Base64.getEncoder()
+                .encodeToString("admin:admin".getBytes(StandardCharsets.UTF_8));
+        final var request = MockMvcRequestBuilders.get("/api/deals")
+                .header("Authorization", "Basic " + basicPayload);
+
+        mockMvc.perform(request)
+                .andExpect(status().is2xxSuccessful())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$").isNotEmpty())
-                .andExpect(jsonPath("$[0].id").value(id.toString()));
+                .andExpect(jsonPath("$[0].id").value(id.toString()))
+                .andExpect(content().json("[]"));
     }
 }
