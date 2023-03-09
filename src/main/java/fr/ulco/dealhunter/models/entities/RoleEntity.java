e8097fc22a79f2361be08a3c38c145e03c7338d3
Axel Lebas <lebas.axel@gmail.com>
2023-03-09 15:56:56 +0100

    refactor: roles

--- src/main/java/fr/ulco/dealhunter/models/entities/RoleEntity.java
+++ src/main/java/fr/ulco/dealhunter/models/entities/RoleEntity.java
@@ -10,9 +10,8 @@ import org.springframework.security.core.GrantedAuthority;
 @NoArgsConstructor
 public class RoleEntity implements GrantedAuthority {
 
-    public static final String USER_ADMIN = "USER_ADMIN";
-    public static final String AUTHOR_ADMIN = "AUTHOR_ADMIN";
-    public static final String BOOK_ADMIN = "BOOK_ADMIN";
+    public static final String USER = "USER";
+    public static final String ADMIN = "ADMIN";
 
     private String authority;
 }
